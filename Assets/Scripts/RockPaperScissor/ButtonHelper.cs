4aa3899b12a86ee8909db0e0da5ddb88d9487221
Olav Vatne <olavvatne@gmail.com>
2017-04-29 17:15:31 +0200

    Renames icons

--- Assets/Scripts/RockPaperScissor/ButtonHelper.cs
+++ Assets/Scripts/RockPaperScissor/ButtonHelper.cs
@@ -6,18 +6,35 @@ using UnityEngine.UI;
 
 public class ButtonHelper : MonoBehaviour {
 
-	public string controller = "Xbox 360";
+	public string controller = "360";
 	public string controlButton = "X";
+	public string keyboardButton = "1";
 	public string buttonText = "Button";
 	private Text _label;
 	private RawImage _icon;
 	// Use this for initialization
 	void Start () {
+		// TODO: need to dynamically set helper
+		bool isJoy = CheckIfJoystick();
+		string btnName = controlButton;
+		string iconPrefix = controller;
+		if (!isJoy) {
+			controller = "KeyboardMouse/Dark";
+			iconPrefix = "Keyboard_Black";
+			btnName = keyboardButton;
+		}
+
 		_label = GetComponentInChildren<Text>();
 		_icon = GetComponentInChildren<RawImage>(); 
+
 		_label.text = buttonText;
-		string path = "ControllerIcons/" + controller + "/360_" + controlButton;
+		string path = "ControllerIcons/" + controller + "/"+ iconPrefix + "_" + btnName;
+		Debug.Log(path);
 		_icon.texture = Resources.Load<Texture2D>(path);
 	}
 	
+		// TODO: fix this at some point
+	bool CheckIfJoystick() {
+		return Input.GetJoystickNames().Length > 0 ? true : false;
+	}
 }
--- Assets/Scripts/RockPaperScissor/HandController.cs
+++ Assets/Scripts/RockPaperScissor/HandController.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
     left = -90, right = 90, top = -180, bottom = 0
  }
 
+ public enum HandState {
+    rock, paper, scissor
+ }
 public class HandController : MonoBehaviour {
 
 	public PlayerData _data;
@@ -17,8 +20,9 @@ public class HandController : MonoBehaviour {
 	public Texture paper;
 	public HandPosition current = HandPosition.left;
 	private Text _playerName;
-	private Texture[] _handTexs;
-	private int idx = 0;
+
+	private HandState _currentHandState = HandState.rock;
+	private bool isJoystick = false;
 
 	// Use this for initialization
 	void Start () {
@@ -28,18 +32,28 @@ public class HandController : MonoBehaviour {
 		_hand = transform.Find("Hand").gameObject;
 		_handImage = _hand.GetComponent<RawImage>();
 		SetHandOrientation();
-		
-		 _handTexs = new Texture[3] { rock, scissor, paper };
 	}
 	
 	private void SetHandOrientation() {
 		_hand.GetComponent<RectTransform>().rotation =  Quaternion.Euler(0,0, (float)current);
 	}
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButton("Fire1")) {
-			_handImage.texture = _handTexs[idx];
-			idx = (idx + 1) % _handTexs.Length;
+			// A button and paper
+			_handImage.texture = paper;
+			_currentHandState = HandState.paper;
+		}
+		else if(Input.GetButton("Fire2")) {
+			// B button and scissor
+			_handImage.texture = scissor;
+			_currentHandState = HandState.scissor;
+		}
+		else if(Input.GetButton("Fire3")) {
+			// X button and Rock
+			_handImage.texture = rock;
+			_currentHandState = HandState.rock;
 		}
 	}
 }
