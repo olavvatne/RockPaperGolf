bc0bcedb440594ca4e4bb9ab4120a4f552b56ad7
Olav Vatne <olavvatne@gmail.com>
2017-05-04 21:47:17 +0200

    Countdown and timeout sound

--- Assets/Scripts/RockPaperScissor/CountDown.cs
+++ Assets/Scripts/RockPaperScissor/CountDown.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CountDown : MonoBehaviour {
 
+	public AudioClip tick;
+	public AudioClip timeout;
 	private Text _text;
+	private AudioSource _source;
+	
 
 	// Use this for initialization
 	void Start () {
 		_text = GetComponent<Text>();
 		_text.text = "";
+
+		_source = GetComponent<AudioSource>();
 	}
 	
 	public IEnumerator StartCountDown(int startCount) {
 		_text.text = startCount + "";
 		for(int i = startCount; i>0; i--) {
 			_text.text = i + " ";
+			_source.PlayOneShot(tick);
 			yield return new WaitForSeconds(1);
 		}
+		_source.PlayOneShot(timeout);
 		_text.text = "";
 	}
 
